93614792f69bf2c9c216a8f81126991c26f75931
Eerthem <eerthem22@gmail.com>
2020-03-28 11:57:33 -0700

    initial commit

---
+++ js/main.js
@@ -0,0 +1,135 @@
+jQuery(function ($) {
+	'use strict',
+
+		$(function () {
+			$('#myCarousel').carousel({
+				interval: 5000000
+			});
+			var id = 0;
+			$('#carousel-text').html($('#slide-content-' + id).html());
+			$('[id^=carousel-selector-]').click(function () {
+				id = this.id.substring(this.id.lastIndexOf('-'));
+				id = parseInt(id.replace('-', ''));
+				$('#myCarousel').carousel(id);
+			});
+			$('#myCarousel').on('slid.bs.carousel', function (e) {
+				id = $('#myCarousel .active').index('#myCarousel .item');
+				$('#carousel-text').html($('#slide-content-' + id).html());
+			});
+
+		});
+$(document).ready(function(){
+	$(function () {
+
+		$(document).on('scroll', function () {
+
+			if ($(window).scrollTop() > 100) {
+				$('.scroll-top-wrapper').addClass('show');
+			} else {
+				$('.scroll-top-wrapper').removeClass('show');
+			}
+		});
+
+		$('.scroll-top-wrapper').on('click', scrollToTop);
+	});
+
+	function scrollToTop() {
+		verticalOffset = typeof (verticalOffset) != 'undefined' ? verticalOffset : 0;
+		element = $('body');
+		offset = element.offset();
+		offsetTop = offset.top;
+		$('html, body').animate({ scrollTop: offsetTop }, 500, 'linear');
+	}
+});
+	// accordian
+	$('.accordion-toggle').on('click', function () {
+		$(this).closest('.panel-group').children().each(function () {
+			$(this).find('>.panel-heading').removeClass('active');
+		});
+
+		$(this).closest('.panel-heading').toggleClass('active');
+	});
+
+	//Initiat WOW JS
+	new WOW().init();
+
+	// portfolio filter
+	$(window).load(function () {
+		'use strict';
+		var $portfolio_selectors = $('.portfolio-filter >li>a');
+		var $portfolio = $('.portfolio-items');
+		$portfolio.isotope({
+			itemSelector: '.portfolio-item',
+			layoutMode: 'fitRows'
+		});
+
+		$portfolio_selectors.on('click', function () {
+			$portfolio_selectors.removeClass('active');
+			$(this).addClass('active');
+			var selector = $(this).attr('data-filter');
+			$portfolio.isotope({ filter: selector });
+			return false;
+		});
+	});
+
+	// Contact form
+	var form = $('#main-contact-form');
+	form.submit(function (event) {
+		event.preventDefault();
+		var form_status = $('<div class="form_status"></div>');
+		$.ajax({
+			url: $(this).attr('action'),
+
+			beforeSend: function () {
+				form.prepend(form_status.html('<p><i class="fa fa-spinner fa-spin"></i> Email is sending...</p>').fadeIn());
+			}
+		}).done(function (data) {
+			form_status.html('<p class="text-success">' + data.message + '</p>').delay(3000).fadeOut();
+		});
+	});
+
+
+	//goto top
+	$('.gototop').click(function (event) {
+		event.preventDefault();
+		$('html, body').animate({
+			scrollTop: $("body").offset().top
+		}, 500);
+	});
+
+	//Pretty Photo
+	$("a[rel^='prettyPhoto']").prettyPhoto({
+		social_tools: false
+	});
+
+$(document).on('click', '.panel-heading span.clickable', function (e) {
+    var $this = $(this);
+    if (!$this.hasClass('panel-collapsed')) {
+        $this.parents('.panel').find('.panel-body').slideUp();
+        $this.addClass('panel-collapsed');
+        $this.find('i').removeClass('glyphicon-minus').addClass('glyphicon-plus');
+    } else {
+        $this.parents('.panel').find('.panel-body').slideDown();
+        $this.removeClass('panel-collapsed');
+        $this.find('i').removeClass('glyphicon-plus').addClass('glyphicon-minus');
+    }
+});
+$(document).on('click', '.panel div.clickable', function (e) {
+    var $this = $(this);
+    if (!$this.hasClass('panel-collapsed')) {
+        $this.parents('.panel').find('.panel-body').slideUp();
+        $this.addClass('panel-collapsed');
+        $this.find('i').removeClass('glyphicon-minus').addClass('glyphicon-plus');
+    } else {
+        $this.parents('.panel').find('.panel-body').slideDown();
+        $this.removeClass('panel-collapsed');
+        $this.find('i').removeClass('glyphicon-plus').addClass('glyphicon-minus');
+    }
+});
+$(document).ready(function () {
+    $('.panel-heading span.clickable').click();
+    $('.panel div.clickable').click();
+});
+
+
+});
